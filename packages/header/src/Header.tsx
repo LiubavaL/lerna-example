f13967a1ae4729dbaa29250f9297d9a10d7365c6
LiubavaL <liuba.vitko@gmail.com>
2025-11-01 22:18:30 +0000

    feat(header): some feature

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Header() {
-  return <header>Lerna is the original monorepo tool! sdfsdfsdf </header>;
+  return <header>Lerna is the original monorepo tool! dd </header>;
 }
