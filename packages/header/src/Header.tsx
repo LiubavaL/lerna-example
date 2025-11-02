089e2c06bb7f018f618902a9d998ce8371ba68b8
LiubavaL <liuba.vitko@gmail.com>
2025-11-02 08:25:33 +0000

    feat: fix a bug

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Header() {
-  return <header>Lerna is the orxcxginal monorepo tool! </header>;
+  return <header>Lerna is the orxcxginal monorepo tool! sdsd</header>;
 }
